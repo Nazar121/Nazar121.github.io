30b4b1d7c974149249e031251d5f95141989fc9f
Nazar Burban <BurbaNazar98@gmail.com>
2018-05-15 22:23:42 +0300

    added icons for technologies

--- src/js/main.js
+++ src/js/main.js
@@ -39,7 +39,7 @@ const technologies = {
         img: 'dist/img/technologies/angular.svg',
     },
     redux: {
-        name: 'REDUX',
+        name: 'Redux',
         img: 'dist/img/technologies/redux.svg',
     },
     firebase: {
@@ -83,7 +83,7 @@ const technologies = {
         img: 'dist/img/technologies/less.svg',
     },
     sass: {
-        name: 'SASS',
+        name: 'SCSS',
         img: 'dist/img/technologies/sass.svg',
     }
 };
@@ -95,7 +95,7 @@ let portfolio = [
         url: 'https://weather-com.firebaseapp.com',
         class: 'weather',
         img: 'dist/img/portfolio__project4.jpg',
-        technologies: [technologies.angular, technologies.redux, technologies.ts, technologies.firebase, technologies.npm, technologies.bootstrap, technologies.html, technologies.sass],
+        technologies: [technologies.angular, technologies.redux, technologies.ts, technologies.firebase, technologies.git, technologies.npm, technologies.bootstrap, technologies.html, technologies.css, technologies.sass],
         adaptive: true,
         github: 'https://github.com/Nazar121/app-weather'
     },
@@ -191,7 +191,7 @@ function project(){
     // силка на GitHyb
     $('.info__github').html(`<span>GitHub</span>: <a href="${portfolio[index].github}" target="_blank">Link to this project.</a>`);
     
-    // записую адаптивний проект так/ні
+    // записую адаптивний так/ні
     if (portfolio[index].adaptive){
         $('.info__adaptive').html(`<span>Responsive design</span>: Yes.`);
     }else{
@@ -200,7 +200,11 @@ function project(){
     
     // записую технології які використовував
     portfolio[index].technologies.map(obj => {
-        result += `<img src="${obj.img}" alt="${obj.name}" class="img"/>`;
+        result += `
+        <div class="technology">
+            <img src="${obj.img}" alt="${obj.name}" class="technology__img"/>
+            <div class="technology__desc">${obj.name}</div>
+        </div>`;
     });
     $('.info__tehnology').empty();
     $('.info__tehnology').append(`<span>Used technologies</span>: ${result}`);
@@ -208,160 +212,3 @@ function project(){
     // Записую URL адресу для проекта 
     $('.info__show').attr('href',portfolio[index].url);
 }
-
-/*----------------------------------------------------------------*/
-// Скролл меню
-let height = document.documentElement.clientHeight;
-let width = document.documentElement.clientWidth + 17;
-let heightMenu = height + 120;
-//console.log(height);
-//console.log(width);
-//console.log(window.pageYOffset);
-
-setInterval(scrollMenu, 10);
-
-function scrollMenu (e){
-    if (width >= 1200){
-        if(window.pageYOffset >= 801) {
-            $('.header__menuPosition').css({
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                height: '50px'
-            });
-        }else {
-            $('.header__menuPosition').css({
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                top: '94%',
-                height: '50px'
-            });
-        }
-    }else if (width >= 992 && width < 1200){
-        if(window.pageYOffset >= 615) {
-            $('.header__menuPosition').css({
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                height: '50px'
-            });
-        }else {
-            $('.header__menuPosition').css({
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                top: '92.4%',
-                height: '50px'
-            });
-        }
-    }/*else {
-        console.log('<768');
-        //$('.menuScroll').show(100);
-        if(window.pageYOffset >= 120) {
-            $('.menuScroll').show(100);
-            $('.toUp').show(100);
-            //console.log('YES');
-        }else {
-            $('.menuScroll').hide(100);
-            $('.toUp').hide(100);
-        }
-    } */ 
-}
-
-/*setInterval(function(){
-    console.log(window.pageYOffset);
-}, 10);*/
-
-
-// Валідація форми
-//  email
-let arrUser = [];
-let boolEmail = false;
-let boolName = false;
-function validEmail(){
-    let email = /^([a-z0-9\.-]{1,}){1,}@([a-z]{1,})\.(ua|com|com\.ua)$/i;
-    
-    $(".contact__inputEmail").on("blur",function(){
-        if ($(this).val().match(email)){
-            $(".contact__err2").hide();
-            $(".contact__good2").show();
-            boolEmail = true;
-        } 
-        else if ($(this).val() == ' '){
-            $(".contact__err2").show();
-            $(".contact__good2").hide();
-            boolEmail = false;
-        }
-        else{
-            $(".contact__err2").show();
-            $(".contact__good2").hide();
-            boolEmail = false;
-        }
-    });
-    
-    return boolEmail;
-}
-validEmail();
-
-//  name
-function validName(){
-    let regName = /^([a-z]+)$/i;
-    
-    $('.contact__inputName').on('blur',function(){
-        if ($(this).val().match(regName)){
-            $(".contact__err").hide();
-            $(".contact__good").show();
-            boolName = true;
-        } 
-        else if ($(this).val() == ' '){
-            $(".contact__err").show();
-            $(".contact__good").hide();
-            boolName = false;
-        }
-        else{
-            $(".contact__err").show();
-            $(".contact__good").hide();
-            boolName = false;
-        }
-    });
-    
-    return boolName;
-}
-validName();
-
-// add sendMess
-function sendEmail(){
-    $('.contact__sendMes').on('click', function(){
-        if(validEmail() && validName){
-            alert('Success');
-            let obj = {};
-            obj.name = $('.contact__inputName').val();
-            obj.emaile = $('.contact__inputEmail').val();
-            obj.massage = $('.contact__inputMessage').val();
-            arrUser.push(obj);
-            $('.contact__inputName').val('');
-            $('.contact__inputEmail').val('');
-            $('.contact__inputMessage').val('');
-            $(".contact__good").hide();
-            $(".contact__good2").hide();
-        }else {
-            alert('Wrrong data');
-        }
-    });
-} 
-sendEmail();
-
-
-// mobile menu
-$('.top__openMenu').on('click', function(){
-    $(this).hide();
-    $('.top__closeMenu').show();
-    $('.sm__toggleMenu').show(50);
-});
-
-$('.top__closeMenu').on('click', function(){
-    $(this).hide();
-    $('.top__openMenu').show();
-    $('.sm__toggleMenu').hide(50);
-});
